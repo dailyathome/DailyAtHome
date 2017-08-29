b63b1de30e6d6eb3c07be2d2a4d85d40ace8a1dc
kiran <kirankatnam485@gmail.com>
2017-08-28 23:31:29 -0500

    style improvements

--- DailyAtHome.Web/src/app/services/spinner.service.ts
+++ DailyAtHome.Web/src/app/services/spinner.service.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 
 export class SpinnerService {
-    public spinnerCounter: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+    public spinnerCounter: BehaviorSubject<number> = new BehaviorSubject<number>(1);
     displaySpinner(value: boolean) {
         let counter = value ? this.spinnerCounter.value + 1 : this.spinnerCounter.value - 1;
         this.spinnerCounter.next(counter);
--- DailyAtHome.Web/src/app/spinner/spinner.component.ts
+++ DailyAtHome.Web/src/app/spinner/spinner.component.ts
@@ -1,10 +1,13 @@
 ﻿import { Component } from '@angular/core';
 
+
+//@Directive({ selector: '[loadingModal]' })
+
 @Component({
     selector: 'spinner',
     templateUrl: 'spinner.component.html',
-    styleUrls: ['spinner.component.css'] 
+    styleUrls: ['spinner.component.css']
 })
-export class SpinnerComponent {
+export class SpinnerComponent  {
 
 }
